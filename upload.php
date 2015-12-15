81bab534d59a5e9dcf3901b19c2c5e5e645e15b9
Marcus Vinicius Gonçalves <marquinhusgoncalves@hotmail.com>
2015-12-15 11:20:51 -0200

    Pequenas mudanças e add do readme

--- upload.php
+++ upload.php
@@ -16,13 +16,13 @@ $arquivo = isset($_FILES["arquivo"]) ? $_FILES["arquivo"] : FALSE;
 
 {
 // ler extensão de arquivo
-preg_match("/\.(gif|bmp|png|jpg|jpeg|exe|zip|txt|mp3|mpg|wav|wma|doc|xls|ppt|html){1}$/i", $arquivo["name"], $ext);
+preg_match("/\.(gif|bmp|png|jpg|jpeg|exe|zip|txt|mp3|mpg|wav|wma|doc|xls|ppt|html|tif){1}$/i", $arquivo["name"], $ext);
 
 // Renomear arquivo para evitar quebras
 $file_nome = md5(uniqid(time())) . "." . $ext[1];
 
 // upload e registro de pasta
-$file_dir = "/home/mundosa/public_html/sites/up/arquivos/" . $file_nome;
+$file_dir = "/home/u300339450/public_html/upload/arquivos/" . $file_nome;
 
 // Upload e alocação de arquivo
 move_uploaded_file($arquivo["tmp_name"], $file_dir);
